cf5527891e532af1e85fcb26e07f07e3e59872e5
Lehlohonolo Seokwe <32381965+Seokwe@users.noreply.github.com>
2018-03-26 05:03:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+	</title>
+</head>
+<body>
+
+	<form action="includes/signup.php" method="POST">
+		<input type="email" name="email" placeholder="email..."><br><br>
+		<input type="password" name="pwd" placeholder="password"><br><br>
+		<button type="submit" name="submit">SUBMIT</button>
+	</form>
+
+</body>
+</html>
